9c47c52fdda98935a1c30ea8c14275e1f21f728f
farewell12345 <1149558764@qq.com>
2021-09-07 16:47:29 +0800

    stopWords

--- Train/__init__.py
+++ Train/__init__.py
@@ -1,23 +1,22 @@
 from typing import Dict, List
 
-import jieba as jieba
-import spacy
-
+import jieba
+import jieba.posseg as pseg
 from Models.Answer import Answer
 from Models.Question import Question
 
-nlp = spacy.load('en_core_web_sm')
+jieba.enable_parallel(4)
 
 
-def move_stop_words(sentence: str) -> List[str]:
+def move_stop_words(sentence: str) -> str:
     sentence = jieba.cut(sentence, cut_all=False)
-    return sentence
+    return ''.join(sentence)
 
 
 def train_data(data: Dict[Question, Answer]):
     for question, answer in data.items():
-
-        doc = nlp(question.toString())
-        for word in doc:
-            print(word)
+        doc = move_stop_words(question.toString())
+        words = pseg.cut(doc)
+        for word in words:
+            print(word.word, word.flag)
     pass
